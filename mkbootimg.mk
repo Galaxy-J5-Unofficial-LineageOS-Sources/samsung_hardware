3ab584d590fc0c0993d6ace4394fa09837bf0c22
David <davidmolinamorales1999@gmail.com>
2022-09-17 23:33:26 +0200

    dtbtool: Move to device tree

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -17,7 +17,7 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(strip $(BOARD_KERNEL_SEPARATED_DT)),true)
 ifneq ($(strip $(BOARD_KERNEL_PREBUILT_DT)),true)
 ifeq ($(strip $(BUILD_TINY_ANDROID)),true)
-include system/tools/dtbtool/Android.mk
+include device/samsung/msm8916-common/dtbtool/Android.mk
 endif
 
 ifeq ($(strip $(TARGET_CUSTOM_DTBTOOL)),)
